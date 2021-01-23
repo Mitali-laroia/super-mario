91a9fe4ce0a5cde7c853319fa158ba8b972d4023
Mitali-laroia <04042001mitali@gmail.com>
2021-01-23 11:45:57 +0530

    First Commint

---
+++ game.js
@@ -0,0 +1,44 @@
+//SOLID
+const render={
+    init(gameObj){
+        //drawsky
+        gameObj.tool.fillStyle = "#3498db";
+        gameObj.tool.fillRect(0,0,window.innerWidth,window.innerHeight);
+        gameObj.tool.drawImage(castleImage,40,40,200,150);
+        
+    }
+}
+
+
+class Game{
+    //game basic set up create
+    init(){
+        const canvas = document.querySelector(".board");
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        const tool = canvas.getContext("2d");
+
+        let gameObj = {
+            tool,canvas
+        }
+        render.init(gameObj);
+    }
+    run(){
+        //game execution
+    }
+    reset(){
+        location.reload();
+    }
+}
+
+preload().then(function(){
+    console.log(castleImage);
+    console.log(cloudsImage);
+    console.log(mountainImage);
+    console.log(spriteSheetImage);
+    console.log(tilesetImage);
+    console.log("Now game will start");
+
+    const game = new Game();
+game.init();
+})
